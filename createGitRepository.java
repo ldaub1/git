ee8f283395b87b8c69cc4d59a1632db546ae201f
Lauren Daub <ldaub1@hwemail.com>
2025-09-17 10:31:38 -0700

    can create the directory now + tester :D

--- createGitRepository.java
+++ createGitRepository.java
@@ -1,13 +1,56 @@
 import java.io.*;
 import java.nio.file.*;
 import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.ArrayList;
 
 public class createGitRepository {
 
-    public void generateGitDirectory() {
-        File dir = new File("git");
-        if (!dir.exists())
+    private static File dir = new File("git");
+    private static File objects = new File("git/objects");
+    private static File index = new File("git/index");
+
+    public static void generateGitDirectory() {
+        if (!dir.exists() && !objects.exists() && !index.exists()) {
             dir.mkdir();
+            objects.mkdir();
+            try {
+                index.createNewFile();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        } else {
+            System.out.println("Git Repository Already Exists");
+        }
+
     }
+    
+    public static void commit(String fileName) {
+
+
+        String data = getFileContents(fileName);
+
+        String sha1Hash = generateSHA1Hash(data);
+
+        // 1. Using BufferedWriter
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("git/index"))) {
+            bufferedWriter.write(sha1Hash);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getFileContents(String fileName) {
+        StringBuilder data = new StringBuilder("");
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                data.append(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return data.toString();
+	}
     
 }
--- projectTester.java
+++ projectTester.java
@@ -1,3 +1,33 @@
+import java.io.*;
+import java.nio.file.*;
+import java.nio.charset.StandardCharsets;
+
 public class projectTester {
-    
+
+    public static void main(String[] args) {
+        testGenerateGitDirectoryy();
+    }
+
+    public static void testGenerateGitDirectoryy() {
+
+        File dir = new File("git");
+        File objects = new File("git/objects");
+        File index = new File("git/index");
+
+        if (index.exists())
+            index.delete();
+        if (objects.exists())
+            objects.delete();
+        if (dir.exists())
+            dir.delete();
+
+        createGitRepository.generateGitDirectory();
+
+        System.out.println("git Directory Created: " + dir.exists());
+        System.out.println("objects Directory Created Inside git Directory: " + objects.exists());
+        System.out.println("index File Created Inside git Directory: " + index.exists());
+
+        System.out.println("\nError Sucessfully Thrown If Exists?");
+        createGitRepository.generateGitDirectory();
+    }
 }
