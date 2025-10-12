82df0f4ccaf8b6e34b697828346650571d2272e4
Miles Aguilar <maguilar1@hwemail.com>
2025-10-12 14:31:44 -0700

    (GP 4.3.15)

--- GitTester.java
+++ GitTester.java
@@ -10,23 +10,16 @@ public class GitTester {
 
         gw.add("testFolder/another/here.txt");
 
-        /*
-         * The hash of this commit is 54f8ee50162ef60047399a1965618ae2cf33d723
-         */
-        gw.commit("miles", "1 commit");
-
+        String hashOfOldCommit = gw.commit("miles", "1 commit");
 
         gw.add("testFolder/one_more.txt");
         gw.add("testFolder/another.txt");
         gw.add("testFolder/shouldbegone.txt");
         
-        /*
-         * The hash of this commit is 52a299566db85e06a6ed71b5ff64216abf67802d
-         */
-        gw.commit("miles", "2 commit");
+        String hashOfNewCommit = gw.commit("miles", "2 commit");
         
-        gw.checkout("54f8ee50162ef60047399a1965618ae2cf33d723");
-        gw.checkout("52a299566db85e06a6ed71b5ff64216abf67802d");
+        gw.checkout(hashOfOldCommit);
+        gw.checkout(hashOfNewCommit);
 
         // in the end, nothing should change
         // we checked out an older commit and then came back to the current one
